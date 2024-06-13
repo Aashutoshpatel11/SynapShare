c80d58fc0e592b76f28f2194cd0fc3a47dc23b9a
Aashutoshpatel11 <146903229+Aashutoshpatel11@users.noreply.github.com>
2024-06-13 13:57:08 +0000

    completed

--- 03_project_TicTacTic/TicTacTic.cpp
+++ 03_project_TicTacTic/TicTacTic.cpp
@@ -2,7 +2,6 @@
 using namespace std;
 
 void print( char game_board[3][3] ){
-    int size = sizeof(game_board)/sizeof(char);
     for( int i=0 ; i<3 ; i++ ){
         cout<<"-------"<<endl;
         for( int j=0 ; j<3 ; j++ ){
@@ -21,20 +20,20 @@ void check( char game_board[3][3] , int &game , char name){
         game_board[0][1]=='x' && game_board[1][1]=='x' && game_board[2][1]=='x' || game_board[0][1]=='o' && game_board[1][1]=='o' && game_board[2][1]=='o' ||
         game_board[0][2]=='x' && game_board[1][2]=='x' && game_board[2][2]=='x' || game_board[0][2]=='o' && game_board[1][2]=='o' && game_board[2][2]=='o' ||
         game_board[0][0]=='x' && game_board[1][1]=='x' && game_board[2][2]=='x' || game_board[0][0]=='o' && game_board[1][1]=='o' && game_board[2][2]=='o' ||
-        game_board[2][0]=='x' && game_board[1][1]=='x' && game_board[0][2]=='x' || game_board[2][0]=='o' && game_board[1][1]=='o' && 1game_board[0][2]=='o')
+        game_board[2][0]=='x' && game_board[1][1]=='x' && game_board[0][2]=='x' || game_board[2][0]=='o' && game_board[1][1]=='o' && game_board[0][2]=='o')
         {
             cout<<"Status: "<<name<<" Won The Game"<<endl;
             game = 1;
         }
-    else if(game_board[0][0]!='-' &&
-            game_board[0][1]!='-' &&
-            game_board[0][2]!='-' &&
-            game_board[1][0]!='-' &&
-            game_board[1][1]!='-' &&
-            game_board[1][2]!='-' &&
-            game_board[2][0]!='-' &&
-            game_board[2][1]!='-' &&
-            game_board[2][2]!='-' )
+    else if(game_board[0][0]!='_' &&
+            game_board[0][1]!='_' &&
+            game_board[0][2]!='_' &&
+            game_board[1][0]!='_' &&
+            game_board[1][1]!='_' &&
+            game_board[1][2]!='_' &&
+            game_board[2][0]!='_' &&
+            game_board[2][1]!='_' &&
+            game_board[2][2]!='_' )
             {
             cout<<"Status: draw"<<endl;
             game = 1;
@@ -47,16 +46,16 @@ int main(){
 
     char game_board[3][3]={{'_','_','_'},{'_','_','_'},{'_','_','_'}};
     int position;
-    int game=0;
+    int game = 0;
     while( game!=1 ){
-        cout<<"Player A input: "<<endl;
+        cout<<endl<<"Player A input: ";
         cin>>position;
         position--;
         game_board [position/3][position%3]='x';
         print(game_board);
         check( game_board , game , 'A' );
 
-        cout<<"Player B input: "<<endl;
+        cout<<endl<<"Player B input: ";
         cin>>position;
         position--;
         game_board [position/3][position%3]='o';
